dacf8d121b4c3974ccdf0a55c76dbf1c8d55616f
Will Rodgers <will.rodgers1995@gmail.com>
2016-08-04 16:06:44 -0400

    Saving progress

--- src/com/theironyard/charlotte/Main.java
+++ src/com/theironyard/charlotte/Main.java
@@ -1,20 +1,46 @@
 package com.theironyard.charlotte;
 
+import jdk.internal.org.objectweb.asm.tree.analysis.Value;
+
+import java.util.HashMap;
 import java.util.Scanner;
 
 public class Main {
 
     public static Scanner scanner = new Scanner(System.in);
-    public static User User = new User();
 
-
+    private static HashMap<String, Double> person = new HashMap<>();
 
 
     public static void main(String[] args) throws Exception {
-    System.out.println("Welcome!");
-    User.chooseName();
-    User.chooseOption();
-//    User.chooseAmount();
+        System.out.println("Welcome! \n Please sign in...");
+
+        // HashMap = <key, value >
+        person.put("Steve", 45.00);
+        person.put("Lisa", 85.00);
+        person.put("Gary", 145.00);
+        person.put("Raymond", 120.00);
+
+
+        // setting scanner to String account
+        String account = scanner.nextLine();
+        // waiting for in. must contain valid key
+        if (person.containsKey(account));{
+            System.out.println("Hello, " + account + " \n Choose an option...");
+        }
+
+        System.out.println("[1] Check Balance \n[2] Withdraw \n[3] Cancel");
+
+        String option = scanner.nextLine();
+        if (option == "1") {
+            System.out.println("Your balance is 100.00");
+        }
+
+
+
+
+
+        // Choosing options
 
     }
 }
--- src/com/theironyard/charlotte/User.java
+++ src/com/theironyard/charlotte/User.java
@@ -1,17 +1,11 @@
 package com.theironyard.charlotte;
 
 public class User {
-    String name;
+
     String option;
     String amount;
 
-    public void chooseName () throws Exception {
-        System.out.println("What is your name?");
-        name = Main.scanner.nextLine();
-        System.out.println("Hello, " + name + "!");
-    }
-        //SHOWING OPTIONS
-    public void chooseOption () throws Exception {
+    public void chooseOption() throws Exception {
         System.out.println("Please choose an option. \n To choose an option, type: \n [ Check Balance ] \n [ Withdraw ] \n [ Cancel ]");
         option = Main.scanner.nextLine();
 
@@ -22,32 +16,24 @@ public class User {
             System.out.println("Transaction canceled... \n Thank you, please come again.");
 
             // WITHDRAWING
-        } else if (option.equalsIgnoreCase("withdraw")){
+        } else if (option.equalsIgnoreCase("withdraw")) {
             System.out.println("Enter withdraw amount:");
             amount = Main.scanner.nextLine();
             int x = Integer.valueOf(amount);
 
-            // hardmode
-            if (x%5==0 || x%10==0 || x%20==0){
+            if (x > 0 && x < 100) {
                 System.out.println("Printing... $" + x + " \n Please remove cash...");
-                System.out.println("\n You have $" + (100 - x) +" left in your account. \n Thank you, please come again.");
-
-                // initial entry
-//            if (x > 0 && x < 100 ){
-//                System.out.println("Printing... $" + x + " \n Please remove cash...");
-//                System.out.println("\n You have $" + (100 - x) +" left in your account. \n Thank you, please come again.");
+                System.out.println("\n You have $" + (100 - x) + " left in your account. \n Thank you, please come again.");
 
             } else {
                 System.err.println("Please enter a valid amount.");
 
             }
 
-        // NO VALID COMMANDS
+            // NO VALID COMMANDS
         } else {
             throw new Exception("Please enter a valid command.");
         }
+
     }
 }
-
-
-
